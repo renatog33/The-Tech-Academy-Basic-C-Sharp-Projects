e269479b5301fadf3e9a975fe1cc7def41bae61b
renatog33 <101902010+renatog33@users.noreply.github.com>
2022-08-04 14:54:21 -0700

    ShippingQuote2

--- Shipping_Quote/ShippingQuote/ShippingQuote/Program.cs
+++ Shipping_Quote/ShippingQuote/ShippingQuote/Program.cs
@@ -12,27 +12,34 @@ namespace ShippingQuote
             Console.WriteLine("Please enter the package weight:");//Enter the package weight
             int packageWeight = Convert.ToInt32(Console.ReadLine());//cast as integare and save the package weight
 
-            Console.WriteLine("Please enter the package width:");//Enter the package weight
-            int packageWidth = Convert.ToInt32(Console.ReadLine());//cast as integare and save the package width
-
-            Console.WriteLine("Please enter the package height:");//Enter the package height
-            int packageHeight = Convert.ToInt32(Console.ReadLine());//cast as integare and save the package height
-
-            Console.WriteLine("Please enter the package length:");//Enter the package length
-            int packageLength = Convert.ToInt32(Console.ReadLine());//cast as integare and save the package length
-
-            int quote = (packageWidth * packageHeight * packageLength * packageWeight) / 100;//create varibale to represent quote calculation
-
-            if (packageWidth + packageHeight + packageLength > 50)//Check to see if the dimensions total greater than 50
+            if (packageWeight > 50) //Check to see if the wieght is greater than 50
             {
                 Console.WriteLine("Package too big to be shipped via Package Express.");
             }
             else
             {
-                Console.WriteLine("Your estimated total for shipping this package is: $" + quote +
+                Console.WriteLine("Please enter the package width:");//Enter the package weight
+                int packageWidth = Convert.ToInt32(Console.ReadLine());//cast as integare and save the package width
+
+                Console.WriteLine("Please enter the package height:");//Enter the package height
+                int packageHeight = Convert.ToInt32(Console.ReadLine());//cast as integare and save the package height
+
+                Console.WriteLine("Please enter the package length:");//Enter the package length
+                int packageLength = Convert.ToInt32(Console.ReadLine());//cast as integare and save the package length
+
+                int quote = (packageWidth * packageHeight * packageLength * packageWeight) / 100;//create varibale to represent quote calculation
+
+                if (packageWidth + packageHeight + packageLength > 50)//Check to see if the dimensions total greater than 50
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                }
+                else
+                {
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + quote +
                     " \nThank you!");
+                }
             }
-            Console.ReadLine();
+                    Console.ReadLine();
         }
     }
 }
