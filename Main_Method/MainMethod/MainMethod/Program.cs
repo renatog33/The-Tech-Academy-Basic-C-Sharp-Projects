71554bfb256f809e8406dc853f003743e856fb31
renatog33 <101902010+renatog33@users.noreply.github.com>
2022-08-08 13:27:49 -0700

    MainMethod

---
+++ Main_Method/MainMethod/MainMethod/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test myObj = new Test();//instantiate the class 
+
+            int result1 = myObj.add(19);
+            Console.WriteLine(result1);
+
+            int result2 = myObj.add(1.9m);
+            Console.WriteLine(result2);
+
+            int result3 = myObj.add("10");
+            Console.WriteLine(result3);
+            Console.ReadLine();
+
+            
+        }
+
+    }
+}
---
+++ Main_Method/MainMethod/MainMethod/Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainMethod
+{
+    public class Test
+    {
+        public int add(int input)//Method #1
+        //In that class, create a method that will take in an integer, create a math 
+        //operation for this integer (addition, subtraction, etc.), then return the answer as an integer.
+        {
+            return input + 5;
+        }
+
+        public int add(decimal input)//Method #2
+        {
+            return Convert.ToInt32(input - 5);
+        }
+
+        public int add(string input)//Method #2
+        {
+            return Convert.ToInt32(input) * 10;
+        }
+    }
+}
