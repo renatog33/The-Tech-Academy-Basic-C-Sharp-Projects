3b571afdbd5dbbd898cc2e5c5fd0147d1eedcf27
renatog33 <101902010+renatog33@users.noreply.github.com>
2022-08-08 10:11:58 -0700

    CallingMethods

---
+++ Calling_Methods/CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select a number to do a math operation on");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            int result1 = Test.add(userNumber);
+            Console.WriteLine(result1);
+
+            int result2 = Test.subtract(userNumber);
+            Console.WriteLine(result2);
+
+            int result3 = Test.multiply(userNumber);
+            Console.WriteLine(result3);
+            Console.ReadLine();
+        }
+        
+    }
+}
---
+++ Calling_Methods/CallingMethods/CallingMethods/test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CallingMethods
+{
+    public static class Test
+    {
+        public static int add(int input)//Method #1
+        {
+            return input + 5;
+        }
+        public static int subtract(int input)//Method #2
+        {
+            return input - 5;
+        }
+        public static int multiply(int input)//Method #3
+        {
+            return input * 5;
+        }
+    }
+}
