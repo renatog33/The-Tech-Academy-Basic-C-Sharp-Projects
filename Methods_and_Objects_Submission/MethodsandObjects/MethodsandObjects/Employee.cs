5ca720e8a2ad49773167cf4365868f20dbf2c0de
renatog33 <101902010+renatog33@users.noreply.github.com>
2022-08-09 15:29:50 -0700

    Methods and Objects Submission Assignment

---
+++ Methods_and_Objects_Submission/MethodsandObjects/MethodsandObjects/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MethodsandObjects
+{
+    // #3 Create another class called Employee and have it inherit from the Person class. 
+    //Give the Employee class a property called Id and have it be of data type int.
+    public class Employee : Person
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Methods_and_Objects_Submission/MethodsandObjects/MethodsandObjects/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MethodsandObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //#4 Inside of the Main method, instantiate and initialize an Employee object
+            // with a first name of “Sample” and a last name of “Student”.
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+
+            //#5 Call the superclass method SayName() on the Employee object.
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
