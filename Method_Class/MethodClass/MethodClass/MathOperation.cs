cf60a49d75f7893f3c185acce3a1b19de6a50f9b
renatog33 <101902010+renatog33@users.noreply.github.com>
2022-08-09 09:35:00 -0700

    MethodClass

---
+++ Method_Class/MethodClass/MethodClass/MathOperation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodClass
+{
+    class MathOperation
+    {
+        public void Math(int num1, int num2)
+        {
+            int result = num1 * 100;
+            Console.WriteLine(num2);//Have the method do a math operation on the first integer and display the second integer to the screen.
+        }
+    }
+}
---
+++ Method_Class/MethodClass/MethodClass/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Input a number:");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Input another number:");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                MathOperation Mult = new MathOperation();//In the Main() method of the console app, instantiate the class.
+                Mult.Math(num1, num2);
+
+                Console.ReadLine();
+            }
+        }   
+}
