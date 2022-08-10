a047b29da272d2b35762b3279f904a585d50f2e9
renatog33 <101902010+renatog33@users.noreply.github.com>
2022-08-10 09:13:18 -0700

    Polymorphism

---
+++ Polymorphism/Polymorphism/Polymorphism/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class Employee : Person, IQuittable //#2 Have your Employee class from the previous drill inherit that interface
+    //and implement the Quit() method in any way you choose.
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+        }
+        public void Quit(Person person)
+        {
+            Console.WriteLine("This perosn has quit.");
+        }
+    }
+}
---
+++ Polymorphism/Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee() { firstName = "Sample", lastName = "Student" };
+
+            employee.SayName();
+            Console.ReadLine();
+
+            IQuittable x = new Employee();
+
+            x.Quit(employee);
+            Console.ReadLine();
+        }
+    }
+}
