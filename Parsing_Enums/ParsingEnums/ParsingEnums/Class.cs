9e79fa478df881da93a9c8daef0f89178240cffc
renatog33 <101902010+renatog33@users.noreply.github.com>
2022-08-10 13:22:57 -0700

    ParsingEnums

---
+++ Parsing_Enums/ParsingEnums/ParsingEnums/Class.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnums
+{
+    class Class
+    {
+        public DaysOfTheWeek Day { get; set; }
+    }
+    public enum DaysOfTheWeek //#1 Create an enum for the days of the week.
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
---
+++ Parsing_Enums/ParsingEnums/ParsingEnums/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Class day = new Class();
+                Console.WriteLine("Enter the current day of the week:");//#2 Prompt the user to enter the current day of the week.
+                string x = Console.ReadLine();
+                string userInput = (char.ToUpper(x[0]) + x.Substring(1));
+                day.Day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), userInput);
+                Console.WriteLine("Yes, " + userInput + " is an actual day of the week.");
+            }
+            catch (System.ArgumentException)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
