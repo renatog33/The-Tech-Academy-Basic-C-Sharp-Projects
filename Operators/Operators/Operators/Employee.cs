4d0a8a8c7b0fe46db61f1604e16820be1aea3635
renatog33 <101902010+renatog33@users.noreply.github.com>
2022-08-10 10:38:14 -0700

    Operators

---
+++ Operators/Operators/Operators/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    public class Employee //#1 Create an Employee class with Id, FirstName and LastName properties.
+    {
+        public int Id { get; set;  }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public static bool operator ==(Employee object1, Employee object2)//#2 In the Employee class,
+       //overload the “==” operator so it checks if two Employee objects are equal by comparing their Id property.
+        {
+            bool compare = (object1.Id == object2.Id);
+            return compare;
+        }
+        //have to include the overload of "!=" to continue on and use what we created for "=="
+        public static bool operator !=(Employee object1, Employee object2)//#2Remember that comparison operators must be overloaded in pairs
+        {
+            bool compare = (object1.Id != object2.Id);
+            return compare;
+        }
+    }
+}
---
+++ Operators/Operators/Operators/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee object1 = new Employee() { Id = 1, FirstName = "John", LastName = "Doe"};
+
+            Employee object2 = new Employee() { Id = 2, FirstName = "Jane", LastName = "Doe" };
+
+
+            Console.WriteLine("Do the two employees share the same Id number?:\n" + (object1 == object2));
+            Console.ReadLine();
+            
+        }
+    }
+}
+
