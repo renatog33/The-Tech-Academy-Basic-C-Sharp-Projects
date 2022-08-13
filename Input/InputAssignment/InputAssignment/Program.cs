8b17d3c36a8d584fabb6625d173493c03bafec0c
renatog33 <101902010+renatog33@users.noreply.github.com>
2022-08-12 17:10:52 -0700

    InputAssignment

---
+++ Input/InputAssignment/InputAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace InputAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: "); //Asks the user for a number.
+            
+            string number = (Console.ReadLine());//Logs that number to a text file.
+
+            File.WriteAllText(@"C:\Users\Act-On Giveaway\Desktop\Log.txt", number);
+
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Act-On Giveaway\Desktop\Log.txt", true))//Prints the text file back to the user.
+            {
+                file.WriteLine("\n" + number);
+            }
+
+            Console.Read();
+        }
+    }
+}
