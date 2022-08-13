a0702a1b62d0fd138fcec17ada166da8244c20ae
renatog33 <101902010+renatog33@users.noreply.github.com>
2022-08-13 11:01:24 -0700

    ConstructorSubmission

---
+++ Constructor_Submission/ConstructorSubmission/ConstructorSubmission/MyClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSubmission
+{
+    public class MyClass
+    {
+        public string nbaTeam;
+        public MyClass() : this("The Portland Trailblazers")
+        {
+        }
+        public MyClass(string team)
+        {
+            nbaTeam = team;
+        }
+    }
+}
---
+++ Constructor_Submission/ConstructorSubmission/ConstructorSubmission/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int year = 1977;//#1 Create a const variable.
+
+            var nickname = "The Blazers"; //#2 Create a variable using the keyword “var.”
+
+            MyClass ripcity = new MyClass(); //#3 Chain two constructors together.
+            Console.WriteLine(ripcity.nbaTeam + " are known as \"{0}\" and won a Championship in {1}.", nickname, year);
+            Console.ReadLine();
+
+        }
+    }
+}
