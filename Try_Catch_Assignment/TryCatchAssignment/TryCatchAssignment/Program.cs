9cdf17c78cc5b5c57e7e49fec0e9a6a9ab6c32cd
renatog33 <101902010+renatog33@users.noreply.github.com>
2022-08-13 15:01:27 -0700

    TryCatch

---
+++ Try_Catch_Assignment/TryCatchAssignment/TryCatchAssignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");//#1 Ask the user for their age.
+            bool validAnswer = false;
+            while (!validAnswer)
+            {
+                try //#3 Exceptions must be handled using “try/catch.”
+                {
+                    int age = Convert.ToInt32(Console.ReadLine());
+                    DateTime birthyear = DateTime.Now.AddYears(-age);
+                    Console.WriteLine("You were born in " + birthyear.Year+"."); //#2 Display the year the user was born.
+                    validAnswer = true;               
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please enter a number."); //Display appropriate error messages if the user enters zero or negative numbers.
+                    validAnswer = false;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("An error occurred.");
+                    Console.ReadLine();
+                    return;
+                }
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
