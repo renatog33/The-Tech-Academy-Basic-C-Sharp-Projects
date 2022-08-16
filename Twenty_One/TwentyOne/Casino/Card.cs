3a04994713851595ab348573bce6ac23e2471f6d
renatog33 <101902010+renatog33@users.noreply.github.com>
2022-08-16 14:57:17 -0700

    TwentyOne

---
+++ Twenty_One/TwentyOne/Casino/Card.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Casino
+{
+    public struct Card//make public so it's accessible to other parts of the program
+    {   
+        public Suit Suit { get; set; }//creating properties - by making public, it is accessible to other parts of the program
+        public Face Face { get; set; }//suit is the enum data type name
+
+        public override string ToString()
+        {
+            return string.Format("{0} of {1}", Face, Suit);
+        }
+    }
+    public enum Suit
+    {
+        Clubs,
+        Diamonds,
+        Hearts,
+        Spades
+    }
+    public enum Face
+    {
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace
+    }
+}
